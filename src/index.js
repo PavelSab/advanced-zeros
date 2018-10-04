22b8bbba0e3fef119094527ad544303de95f335c
Павел <43180298+PavelSab@users.noreply.github.com>
2018-10-04 22:56:59 +0300

    Not Yet

--- src/index.js
+++ src/index.js
@@ -1,3 +1,10 @@
 module.exports = function getZerosCount(number, base) {
   // your implementation
+  let zeros=0;
+  while(number>0){
+    number=Math.floor(number/5);
+    zeros+=number;
+  }
+  return zeros;
+  
 }
